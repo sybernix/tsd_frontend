87d921fc75050cd760fc87a8a2b64aa2386d1710
Lankitha Gallage <lankitha.mailbox@gmail.com>
2020-04-30 23:13:05 +0530

    Updated Commit

--- src/lib/class/data/m_student.js
+++ src/lib/class/data/m_student.js
@@ -1,24 +1,23 @@
-
-import m_contact from './m_contact'
+import m_contact from "./m_contact";
 
 class m_student extends m_contact {
-    constructor() {
-        super();
-        this.id = "";
-        this.reg_no = "";
-        this.reg_date = null;
-        this.end_date = null;
-        this.is_active = false;
-    }
+  constructor() {
+    super();
+    this.id = "";
+    this.reg_no = "";
+    this.reg_date = null;
+    this.end_date = null;
+    this.is_active = false;
+  }
 
-    set(values) {
-        super.set(this);
-        this.id = values.id;
-        this.reg_no = values.reg_no;
-        this.reg_date = values.reg_date;
-        this.end_date = values.end_date;
-        this.is_active = values.is_active;
-    }
+  set(values) {
+    super.set(this);
+    this.id = values.id;
+    this.reg_no = values.reg_no;
+    this.reg_date = values.reg_date;
+    this.end_date = values.end_date;
+    this.is_active = values.is_active;
+  }
 }
 
 export default m_student;
